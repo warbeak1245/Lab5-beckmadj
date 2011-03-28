f6639a7673a05cf5877f43a1e1ba64b1a161faec
Daniel Beckmann <beckmadj@rose-hulman.edu>
2011-03-28 17:18:02 -0400

    added first test case doesnt pass

---
+++ PrimeStuff/Lab5/src/PrimeGeneration.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class PrimeGeneration {
+	public static ArrayList<Integer> generatePrimes(int n){
+		return null;
+	}
+}
---
+++ PrimeStuff/Lab5/src/TestPrimeGeneration.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+
+
+public class TestPrimeGeneration {
+	@Test
+	public void testTwo(){
+		assertEquals(new ArrayList<Integer>(), PrimeGeneration.generatePrimes(2));
+	}
+}
